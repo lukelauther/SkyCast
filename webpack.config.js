9f91155a268e44f631e3fa6d4e53fd206a55aa0d
lukelauther <lukelauther1@gmail.com>
2023-07-17 17:52:32 -0400

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    mode: process.env.NODE_ENV,
+
+    entry: './src/index.js',
+
+    output: {
+        path: path.join(__dirname, 'build'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        proxy: {
+            '/api/': 'http://localhost:3000'
+            // '/build': 'http://localhost:3000'
+        },
+        compress: true,
+        port: 8080,
+        historyApiFallback: true
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: [/\.s[ac]ss$/i, /\.css$/i],
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+
+}
