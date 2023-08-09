a710a9c9faae49a7120c025ff93d246de49bf6e5
lukelauther <lukelauther1@gmail.com>
2023-08-09 15:39:23 -0400

    importing forecast correctly and started styling

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,10 +6,10 @@ export default function Dashboard(props) {
 
     for(let key in props.locationInfo.forecast) {
         days.push(
-            <div key={key} className='flex justify-end items-center'>
-                <p>{props.locationInfo.forecast[key].date}</p>
-                <p>{props.locationInfo.forecast[key].max} / {props.locationInfo.forecast[key].min}</p>
-                <p>{props.locationInfo.forecast[key].description}</p>
+            <div key={key} className='flex justify-center items-center gap-4 mb-4'>
+                <p>{!props.locationInfo.forecast[key].date ? '~' : props.locationInfo.forecast[key].date}</p>
+                <p>{!props.locationInfo.forecast[key].max ? '~' : Math.round(props.locationInfo.forecast[key].max)} °F / {!props.locationInfo.forecast[key].min ? '~' : Math.round(props.locationInfo.forecast[key].min)} °F</p>
+                <p>{!props.locationInfo.forecast[key].description ? '~' : props.locationInfo.forecast[key].description}</p>
             </div>
         )
     }
@@ -17,6 +17,7 @@ export default function Dashboard(props) {
     return (
         <div>
 
+            {/* SEARCH BAR */}
             <div className='flex justify-center items-center mt-10 mx-auto h-20 w-3/5 bg-gray-400 rounded-2xl'>
                 <form onSubmit={props.submitLocation} autoComplete='off'>
                     <label htmlFor="location-search" placeholder='city, state, country' className='text-lg'>Enter Location:</label>
@@ -24,35 +25,51 @@ export default function Dashboard(props) {
                 </form>
             </div>
 
-            <div className='mt-5 mx-auto p-5 bg-gray-500 w-3/5 rounded-2xl'>
-                <p className='flex justify-center items-center pl-8 text-2xl'>{!props.locationName ? '~' : props.locationName} </p>
+            {/* MAIN DASHBOARD */}
+            <div className='grid grid-cols-2 mt-5 mx-auto p-5 bg-gray-500 w-3/5 rounded-2xl'>
 
-                <div className='flex flex-row justify-start items-center ml-10'>
-                    <div>
+                {/* LOCATION NAME */}
+                <p className='flex col-span-2 justify-center items-center pl-8 text-2xl'>{!props.locationName ? '~' : props.locationName} </p>
+
+                {/* LEFT SIDE OF DASHBOARD */}
+                <div className='flex flex-col justify-center items-center'>
+                    <div className='flex justify-center items-center'>
+                        <div> 
                         <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="" className='h-40 w-40' />
-                    </div>
-                    <div>
-                        <p className='text-2xl'>{!props.locationInfo.temp ? 0 : Math.round(props.locationInfo.temp)} °F</p>
-                        <p className='mb-4 mt-2 flex justify-start text-lg'>{!props.locationInfo.description ? '~' : props.locationInfo.description} </p>
-                    </div>
-
-                    <div className='flex flex-col justify-center items-end'>
-                        {days}
+                        </div>
+                        <div>
+                            <p className='text-2xl'>{!props.locationInfo.temp ? 0 : Math.round(props.locationInfo.temp)} °F</p>
+                            <p className='mb-4 mt-2 flex justify-start text-lg'>{!props.locationInfo.description ? '~' : props.locationInfo.description} </p>
+                        </div>
                     </div>
                     
-                </div>
-                
-                <div className='flex flex-row justify-start items-center gap-10 ml-10'>
-                    <div>
-                        <p className='text-lg'>Feels like: {!props.locationInfo.feelsLike ? 0 : Math.round(props.locationInfo.feelsLike)} °F</p>
-                        <p className='mt-4 text-lg'>Winds: {!props.locationInfo.windSpeed ? 0 : Math.round(props.locationInfo.windSpeed)} MPH</p>
-                    </div>
-                    <div>
-                        <p className='text-lg'>High: {!props.locationInfo.high ? 0 : Math.round(props.locationInfo.high)} °F</p>
-                        <p className='mt-4 text-lg'>Low: {!props.locationInfo.low ? 0 : Math.round(props.locationInfo.low)} °F</p>
-                    </div> 
-                </div> 
 
+                    <div className='flex flex-row justify-start items-center gap-10 ml-10'>
+                        <div>
+                            <p className='text-lg'>Feels like: {!props.locationInfo.feelsLike ? 0 : Math.round(props.locationInfo.feelsLike)} °F</p>
+                            <p className='mt-4 text-lg'>Winds: {!props.locationInfo.windSpeed ? 0 : Math.round(props.locationInfo.windSpeed)} MPH</p>
+                        </div>
+                        <div>
+                            <p className='text-lg'>High: {!props.locationInfo.high ? 0 : Math.round(props.locationInfo.high)} °F</p>
+                            <p className='mt-4 text-lg'>Low: {!props.locationInfo.low ? 0 : Math.round(props.locationInfo.low)} °F</p>
+                        </div> 
+                    </div> 
+                </div>
+
+                {/* RIGHT SIDE OF DASHBOARD */}
+                <div className='flex flex-col items-center'>
+                    <div className='mt-6 mb-4'>
+                        <p>5 Day Forecast</p>
+                    </div>
+                    <div className='flex flex-col col-span-2 justify-start items-start'>
+                        {days}
+                        {/* <div id='day1'></div>
+                        <div id='day2'></div>
+                        <div id='day3'></div>
+                        <div id='day4'></div>
+                        <div id='day5'></div> */}
+                    </div>
+                </div>
             </div>
             
         </div>
